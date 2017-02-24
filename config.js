13df909e8250fca8fd49fbba551dd6be1b8c1c67
luanacastrillon <luanacastrillon@gmail.com>
2017-02-24 11:26:45 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAtQIt7_s:APA91bH5y1HJf39q5_T0bBOLbx7z33lvtgs5E2Zz_fJeJeeh47mGWG9D1x3e0tH5yJktEIVFrtkeYit3B0NrPCa5GTGVOUbaFWaNva2LhWi46TxTG0Pmfu3HYQmgYSx2tWVxtU29ppY5'
+};
