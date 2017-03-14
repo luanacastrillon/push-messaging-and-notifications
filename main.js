c8744896551fa2c3b3424d7780aab17da9710646
luanacastrillon <luanacastrillon@gmail.com>
2017-03-14 10:50:01 -0300

    ssssssssfirst commit

--- main.js
+++ main.js
@@ -250,6 +250,8 @@ window.addEventListener('load', function() {
     // Listen to messages from service workers.
     navigator.serviceWorker.addEventListener('message', function(event) {
         console.log("Got reply from service worker: " + event.data);
+        var thissound=document.getElementById('anuncioMP3');
+        thissound.Play();
     });
 
     // Are we being controlled?
